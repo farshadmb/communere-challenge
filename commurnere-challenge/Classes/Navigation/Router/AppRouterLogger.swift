bb4590230d353f9a170938391116aaca6b5f6480
Farshad Mousalou <farshadm90@gmail.com>
2020-06-12 06:53:39 +0430

    - added and implemented AppLogger and AppRouterLogger.

---
+++ commurnere-challenge/Classes/Navigation/Router/AppRouterLogger.swift
@@ -0,0 +1,29 @@
+//
+//  AppRouterLogger.swift
+//  commurnere-challenge
+//
+//  Created by Farshad Mousalou on 6/12/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+import RouteComposer
+
+struct AppRouterLogger: RouteComposer.Logger {
+    
+    /// Logs a message
+    ///
+    /// - Parameters:
+    ///  - message: The `LogMessage` instance
+    func log(_ message: LogMessage) {
+        switch message {
+        case .warning(let message):
+            Logger.warnLog("Router Logger => \(message)", tag: "AppRouterLogger")
+        case .info(let message):
+            Logger.infoLog("Router Logger => \(message)", tag: "AppRouterLogger")
+        case .error(let error):
+            Logger.errorLog("Router Logger => \(error)", tag: "AppRouterLogger")
+        }
+    }
+    
+}
