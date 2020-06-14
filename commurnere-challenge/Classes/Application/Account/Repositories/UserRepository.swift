b9f305d13df1ce2d983d90a54b0fab0046dfc49d
Farshad Mousalou <farshadm90@gmail.com>
2020-06-14 15:51:11 +0430

    - Added: UserRepositoryUseCases Interface. - Added: UserRepository class.

---
+++ commurnere-challenge/Classes/Application/Account/Repositories/UserRepository.swift
@@ -0,0 +1,73 @@
+//
+//  UserRepository.swift
+//  commurnere-challenge
+//
+//  Created by Farshad Mousalou on 6/14/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+/// <#Description#>
+class UserRepository: BaseDataRepository<User> {
+    
+}
+
+// MARK: - UserRepositoryUseCases Implementation.
+extension UserRepository: UserRepositoryUseCases {
+    
+    func fetchAllUser() -> Observable<[User]> {
+        return fetch(type: User.self, sort:(key: "id", ascending:true))
+    }
+    
+    func save(user: User) -> Single<Result<String, Error>> {
+        return save(object: user)
+            .flatMap({ (string) -> PrimitiveSequence<SingleTrait, Result<String,Error>> in
+                return .just(Result { string })
+            })
+            .catchError({ (error) -> PrimitiveSequence<SingleTrait, Result<String, Error>> in
+                .just(Result { throw error })
+            })
+    }
+    
+    func update(user: User) -> Single<Result<String, Error>> {
+        return update(object: user)
+            .flatMap({ (string) -> PrimitiveSequence<SingleTrait, Result<String,Error>> in
+                return .just(Result { string })
+            })
+            .catchError({ (error) -> PrimitiveSequence<SingleTrait, Result<String, Error>> in
+                .just(Result { throw error })
+            })
+    }
+    
+    func delete(user: User) -> Single<Result<String, Error>> {
+        return delete(object: user)
+            .flatMap({ (string) -> PrimitiveSequence<SingleTrait, Result<String,Error>> in
+                return .just(Result { string })
+            })
+            .catchError({ (error) -> PrimitiveSequence<SingleTrait, Result<String, Error>> in
+                .just(Result { throw error })
+            })
+    }
+    
+    func find(username: String) -> Single<User?> {
+        return fetchAllUser()
+            .flatMapLatest({[weak self] (users) -> Observable<User?> in
+                return self?.find(username: username, in: users) ?? .just(nil)
+            })
+        .asSingle()
+    }
+    
+    private func find(username: String,in users: [User]) -> Observable<User?> {
+        return Observable.create { (observer) -> Disposable in
+            
+            observer.onNext(users.first(where: { $0.username.lowercased() == username.lowercased() }))
+            observer.onCompleted()
+            
+            return Disposables.create()
+        }
+    }
+    
+    
+}
---
+++ commurnere-challenge/Classes/Application/Account/Repositories/UserRepositoryUseCases.swift
@@ -0,0 +1,42 @@
+//
+//  UserRepositoryUseCases.swift
+//  commurnere-challenge
+//
+//  Created by Farshad Mousalou on 6/14/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+import RxSwift 
+
+protocol UserRepositoryUseCases {
+    
+    /// Fetch All User
+    ///
+    /// - Returns: The `Obseravable` Type of list of `Users`
+    func fetchAllUser() -> Observable<[User]>
+    
+    /// Save and Add an new `User` object into storage
+    ///
+    /// - Parameter user: the user object that will be stored.
+    /// - Returns: an `Single` type of `Result<String,Error>` indicate that the save operation success or not.
+    func save(user: User) -> Single<Result<String,Error>>
+    
+    /// Update `User` object data into storage
+    ///
+    /// - Parameter user: the user object that will be updated.
+    /// - Returns: an `Single` type of `Result<String,Error>` indicate that operation success or not with User Friendly Message.
+    func update(user: User) -> Single<Result<String,Error>>
+    
+    /// Delete an new `User` object from storage
+    ///
+    /// - Parameter user: the user object that will be stored.
+    /// - Returns: an `Single` type of `Result<String,Error>` indicate that operation success or not with User Friendly Message.
+    func delete(user: User) -> Single<Result<String,Error>>
+    
+    /// Find and return a `User` object if the user exist. otherwise return nil
+    ///
+    /// - Parameter username: the username that must be found.
+    /// - Returns: return a `User` object if the user exist. otherwise return nil
+    func find(username: String) -> Single<User?>
+}
